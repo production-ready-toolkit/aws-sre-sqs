b217800bfcb913b213af180e0041df572d7b2185
Matheus Fidelis <matheus@nanoshots.com.br>
2021-09-19 22:29:08 -0300

    fifo fix

--- sqs.tf
+++ sqs.tf
@@ -1,19 +1,18 @@
 resource "aws_sqs_queue" "main" {
     name                        = format("%s%s", var.name, var.fifo ? ".fifo" : "")
-    fifo_queue                  = var.fifo
 
     max_message_size            = var.max_message_size
     visibility_timeout_seconds  = var.visibility_timeout_seconds
     message_retention_seconds   = var.expiration_time_seconds
     receive_wait_time_seconds   = var.receive_wait_time_seconds
 
-    content_based_deduplication = var.fifo
-
     delay_seconds               = var.delay
 
     kms_master_key_id                   = var.kms_key != "" ? var.kms_key : aws_kms_key.main[0].arn
     kms_data_key_reuse_period_seconds   = var.kms_cache_time
 
+    fifo_queue                  = var.fifo
+    content_based_deduplication = var.fifo
     deduplication_scope         = var.fifo ? var.fifo_deduplication_scope : null
     fifo_throughput_limit       = var.fifo ? var.fifo_throughput_limit : null
 
@@ -37,6 +36,11 @@ resource "aws_sqs_queue" "dlq" {
 
     kms_master_key_id                   = var.kms_key != "" ? var.kms_key : aws_kms_key.main[0].arn
     kms_data_key_reuse_period_seconds   = var.kms_cache_time
+
+    fifo_queue                  = var.fifo
+    content_based_deduplication = var.fifo
+    deduplication_scope         = var.fifo ? var.fifo_deduplication_scope : null
+    fifo_throughput_limit       = var.fifo ? var.fifo_throughput_limit : null
 
     tags                        = var.tags
 }
